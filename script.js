23183e0db215c99a800ae7b12e6e0213088bc3a0
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-03-11 19:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = 0; // Initial Counter Value
+
+document.getElementById("incrementBtn").addEventListener("click", function () {
+    alert("Current value before increment: " + counter); // Show alert with un-incremented value
+    counter++; // Increment the counter
+    document.getElementById("counter").innerText = counter; // Update counter display
+});
